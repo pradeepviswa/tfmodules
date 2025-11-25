27facef7f91e733d662d267dab0c0dbad72c18a1
pradeepviswa <74972223+pradeepviswa@users.noreply.github.com>
2025-11-25 22:07:00 +0530

    Update key_file.tf

--- key_file/key_file.tf
+++ key_file/key_file.tf
@@ -1,19 +1,29 @@
 resource "tls_private_key" "ssh_key" {
-  algorithm   = "RSA"
-  rsa_bits    = 4096
+  algorithm = "RSA"
+  rsa_bits  = 4096
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_key_pair" "generated_key" {
   key_name   = var.key_name
   public_key = tls_private_key.ssh_key.public_key_openssh
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "local_file" "private_key_pem" {
   filename = "${path.module}/${var.key_name}.pem"
   content  = tls_private_key.ssh_key.private_key_pem
-
-  # Prevent overwriting if file already exists
   file_permission = "0600"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 output "key_pair_name" {
