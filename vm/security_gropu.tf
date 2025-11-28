de1923b67a190ca1ce7ed82e70c1e05fcd075991
Pradeep Viswanathan <pradeep.viswa@gmail.com>
2025-11-28 12:40:54 +0530

    updated

--- vm/security_gropu.tf
+++ vm/security_gropu.tf
@@ -16,6 +16,14 @@ resource "aws_security_group" "my_sg" {
     }
   }
 
+    ingress {
+    description = "Allow ICMP ping"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
   egress {
     description = "Allow all outbound"
     from_port   = 0
