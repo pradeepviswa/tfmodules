5f595f1d9ffc63fbafc58ed6b93fbad2a73c3286
Pradeep Viswanathan <pradeep.viswa@gmail.com>
2025-12-09 02:10:30 +0530

    updated

--- vm/key_file.tf
+++ vm/key_file.tf
@@ -13,7 +13,7 @@ resource "aws_key_pair" "generated_key" {
 }
 
 resource "local_file" "private_key_pem" {
-  filename        = "${var.key_path}"
+  filename        = "${pathexpand(var.key_path)}"
   content         = tls_private_key.ssh_key.private_key_pem
   file_permission = "0600"
 
